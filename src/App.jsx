fd7290d148c69246b751ee761f29e59532d5ea68
Filip Woźniak <filip.wozniak98@wp.pl>
2024-12-16 08:57:01 +0100

    created GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 
 function App() {
 	return (
 		<main>
 			<div id='game-container'>
-				<ol id='players'>
-					<Player name="Filip" symbol= "X" />
-					<Player name="Player 1" symbol = "0" />
-				
+				<ol id='players' className='highlight-player'>
+					<Player initialName='Filip' symbol='X' />
+					<Player initialName='Player 1' symbol='0' />
 				</ol>
+				{<GameBoard />}
 			</div>
 		</main>
 	);
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,29 @@
-export default function Player({name,symbol}) {
-    
-    return (
-			<li>
-            <span className='player-name'>{name}</span>
-            <span className='player-symbol'>{symbol}</span>
-			<button>Edit</button>
-			</li>
-		);
+import { useState } from "react";
 
+export default function Player({ initialName, symbol }) {
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState(false);
 
+	function changeNameHandler(event) {
+		setPlayerName(event.target.value);
+	}
+
+	function editNameHandler() {
+		setIsEditing(editing => !editing);
+	}
+	let editablePlayerName = <span className='player-name'>{playerName}</span>;
+	let buttonText = 'Edit';
+
+	if (isEditing) {
+		editablePlayerName = <input type='text' required value={playerName} onChange={changeNameHandler} />;
+		buttonText = 'Save';
+	}
+
+	return (
+		<li>
+			{editablePlayerName}
+			<span className='player-symbol'>{symbol}</span>
+			<button onClick={editNameHandler}>{buttonText}</button>
+		</li>
+	);
 }
